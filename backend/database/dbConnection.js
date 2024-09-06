d45ca8d2a142720dea525d46410bebd808cfb28c
vakadamukesh <mukesh.wissenaire@gmail.com>
2024-09-07 01:51:27 +0530

    changed bcrypt

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "placement_portal",
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Connected to database.");
     })
